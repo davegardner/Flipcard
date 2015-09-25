ab212c397e4518f661016d08ec1a6056a391d644
dave@cascadepixels.com.au <dave@cascadepixels.com.au>
2015-09-25 10:56:30 +1000

    Initial Commit

---
+++ ResourceManifest.cs
@@ -0,0 +1,12 @@
+﻿using Orchard.UI.Resources;
+
+namespace Cascade.FlipCard {
+    public class ResourceManifest : IResourceManifestProvider {
+        public void BuildManifests(ResourceManifestBuilder builder) {
+            var manifest = builder.Add();
+
+            manifest.DefineStyle("FlipCard").SetUrl("flipcard.css");
+            manifest.DefineScript("FlipCard").SetVersion("1.1.1").SetUrl("flipcard.js").SetDependencies("jQuery");
+        }
+    }
+}
